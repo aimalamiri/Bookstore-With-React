650d64a220afd98e012875603e18ee6acb76902d
Aimal Amiri <meaimal@hotmail.com>
2022-06-03 01:09:12 +0430

    Add Books, and Book components

---
+++ src/components/Books.js
@@ -0,0 +1,10 @@
+import Book from './Book';
+
+const Books = () => (
+  <div>
+    <Book title="The Alchemist" author="Paulo Coelho" />
+    <Book title="The Kite Runner" author="Khalid Husseni" />
+  </div>
+);
+
+export default Books;
