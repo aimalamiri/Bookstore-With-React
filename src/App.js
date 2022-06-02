7aeec064ffe8ec8e1cc9d322c6108a3d610f67a8
Aimal Amiri <meaimal@hotmail.com>
2022-06-03 01:25:03 +0430

    Add Categories and Navigation components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import Books from './components/Books';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </div>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const Navigation = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/categories">Categories</Link>
+  </nav>
+);
+
+export default Navigation;
