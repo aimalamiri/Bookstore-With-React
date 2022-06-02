35606453f44ab0c427369e8123ec9086f602df72
Aimal Amiri <meaimal@hotmail.com>
2022-06-03 01:19:47 +0430

    Create the AddBooksForm component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,12 @@
 import Book from './Book';
+import AddBookForm from './AddBookForm';
 
 const Books = () => (
   <div>
     <Book title="The Alchemist" author="Paulo Coelho" />
     <Book title="The Kite Runner" author="Khalid Husseni" />
+
+    <AddBookForm />
   </div>
 );
 
