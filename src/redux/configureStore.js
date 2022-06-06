3dc304152ab8eec0e497ddf3d593bf11c4463947
Aimal Amiri <meaimal@hotmail.com>
2022-06-06 18:51:45 +0430

    Add basic redux configuration files

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import books from './books/books';
+import categories from './categories/categories';
+
+const reducer = combineReducers({
+  books,
+  categories,
+});
+
+const store = configureStore(reducer, {});
+
+export default store;
