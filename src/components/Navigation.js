613f1d3e8cee4151deb03fb1c1869fc6eeac4225
Aimal Amiri <meaimal@hotmail.com>
2022-06-09 14:56:55 +0430

    Add user account icon to Navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,22 @@
 import { Link } from 'react-router-dom';
+import person from './man-person.svg';
 
 const Navigation = () => (
-  <nav className="flex">
-    <h1 className="nav-title">Bookstore CMS</h1>
-    <div className="nav-links">
-      <Link to="/" className="nav-link">
-        BOOKS
-      </Link>
-      <Link to="/categories" className="nav-link">
-        CATEGORIES
-      </Link>
+  <nav className="flex justify-between items-center">
+    <div className="flex">
+      <h1 className="nav-title">Bookstore CMS</h1>
+      <div className="nav-links flex items-center">
+        <Link to="/" className="nav-link">
+          BOOKS
+        </Link>
+        <Link to="/categories" className="nav-link">
+          CATEGORIES
+        </Link>
+      </div>
     </div>
+    <a href="/" className="nav-user">
+      <img src={person} width="17" height="17" alt="person" className="nav-person" />
+    </a>
   </nav>
 );
 
